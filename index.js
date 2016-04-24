ae7b471f3920cdcb7e17318b421f290ecce16d51
fen555 <cofen.yensira@gmail.com>
2016-04-24 12:10:19 +0700

    He

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', function (req, res) {
 
 app.set('port', (process.env.PORT || 5000))
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(app.get('port'), function () {
+  console.log('Example app listening on port ' + app.get('port') + '!')
 })
