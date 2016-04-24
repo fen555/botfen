df4da224cf446164939097ab9c3671d1eb4b2046
fen555 <cofen.yensira@gmail.com>
2016-04-24 14:08:16 +0700

    He

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var app = express()
+var text
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -10,6 +11,20 @@ app.get('/webhook/', function (req, res) {
     res.send(req.query['hub.challenge'])
   }
   res.send('Error, wrong validation token')
+})
+
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i]
+    sender = event.sender.id
+    if (event.message && event.message.text) {
+      text = event.message.text
+      // Handle a text message from this sender
+      console.log(text)
+    }
+  }
+  res.sendStatus(200)
 })
 
 app.set('port', (process.env.PORT || 5000))
