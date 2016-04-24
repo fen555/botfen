caf971354d96d092ea4f9ee8a229a1bc25234cba
fen555 <cofen.yensira@gmail.com>
2016-04-24 14:21:24 +0700

    He

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var request = require('request')
 var app = express()
 var text
+var token = 'CAABcdjl6yssBABaft6BjjeZBWQ2v0pOvEUYE0iBFakzJXHE6cPIgyOQi1BlLO6ldZAT9FmX8WR94UXVZB6bIBLQBVDNPaA37sYUIPdHzL225solH4jkLmFZCZBtxsnndfvSzb3lD8BTMFLGxY4nErcOzAdmC1Phys7ZBsUWEySz9JIf6c28jkha0GN2LRsXUpxSfnCTV1JFwZDZD'
 
 app.use(bodyParser.json())
 
@@ -25,6 +27,7 @@ app.post('/webhook/', function (req, res) {
       text = event.message.text
       // Handle a text message from this sender
       console.log(text)
+      sendTextMessage(sender, 'Text received, echo: ' + text.substring(0, 200))
     }
   }
   res.sendStatus(200)
@@ -35,3 +38,24 @@ app.set('port', (process.env.PORT || 5000))
 app.listen(app.get('port'), function () {
   console.log('Example app listening on port ' + app.get('port') + '!')
 })
+
+function sendTextMessage (sender, text) {
+  var messageData = {
+    text: text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: token},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: messageData
+    }
+  }, function (error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error)
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error)
+    }
+  })
+}
