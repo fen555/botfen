edeb636df74e18f2e66b524e3c5fc8d30c19d8e5
fen555 <cofen.yensira@gmail.com>
2016-05-29 10:14:36 +0700

    He

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.post('/webhook/', function (req, res) {
         var gettext = text.substring(4, text.length)
         console.log('text : ' + gettext)
         num = gettext.split(' ')
-        console.log('split : ' + num)
+        console.log('split : ' + num + ' len = ' + num.length)
         for (var i = 0;i < num.length;i++) {
           sum += parseFloat(num[i])
         }
