a3ad46ecbfe9805134d12b99cb23db37e34598c5
fen555 <cofen.yensira@gmail.com>
2016-05-29 10:53:52 +0700

    He

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ var count = 0
 
 app.use(bodyParser.json())
 app.use(express.static('index.html'))
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// app.get('/', function (req, res) {
+//   res.send('Hello World!')
+// })
 
 app.get('/webhook/', function (req, res) {
   if (req.query['hub.verify_token'] === '1234') {
