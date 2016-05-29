faf2bc351cda205d72c7e5a700516a67125f9bc0
fen555 <cofen.yensira@gmail.com>
2016-05-29 09:40:40 +0700

    He

--- index.js
+++ index.js
@@ -47,7 +47,21 @@ app.post('/webhook/', function (req, res) {
         console.log('sum : ' + sum)
         sendTextMessage(sender, 'sum : ' + sum)
       }
-      
+
+      if (getFunc === 'max') {
+        var gettext = text.substring(4, text.length)
+        console.log('number : ' + gettext)
+        var space = gettext.search(' ')
+        var num1 = parseFloat(gettext.substring(0, space))
+        var num2 = parseFloat(gettext.substring(space, gettext.length))
+        console.log('number1 : ' + num1 + ' number2 : ' + num2)
+        if (num1 > num2) {
+          sendTextMessage(sender, 'max : ' + num1)
+        }
+        if (num2 > num1) {
+          sendTextMessage(sender, 'max : ' + num2)
+        }
+      }
     }
   }
   res.sendStatus(200)
